12023e2a458fbaa338f123a106fa3fc1e8da8033
Alejandro <alejandrochile5@gmail.com>
2022-12-30 17:18:53 -0500

    Gid darkens slightly after every passover

--- javascript.js
+++ javascript.js
@@ -11,6 +11,23 @@ function ColorBox(box, color){
     box.style.backgroundColor = color;
 }
 
+function DarkenBox(box){
+    let shade = box.style.backgroundColor;
+    shade = shade.substr(4);
+    shade = shade.substr(0, shade.length-1);
+    shade = shade.split(",");
+    for(let i = 0; i < shade.length; i++){
+        if(shade[i] > 25){
+            shade[i] = shade[i] - 25;
+        }
+        else{
+            shade[i] = 0;
+        }
+    }
+
+    box.style.backgroundColor = `rgb(${shade[0]},${shade[1]},${shade[2]})`;
+}
+
 function PromptForSize() {
     let size = prompt("Set grid size between 1 and 100");
     if (size == null || isNaN(size) || size < 0 || size > 100){
@@ -31,7 +48,8 @@ function ChangeGridSize(size) {
     let numSquares = size*size;
     for (let i = 0; i < numSquares; i++){
         const box = document.createElement('div');
-        box.addEventListener('mouseover', function() {ColorBox(box, 'black')});
+        box.style.backgroundColor = 'rgb(255,255,255)';
+        box.addEventListener('mouseover', function() {DarkenBox(box)});
 
         grid.appendChild(box);
     }
